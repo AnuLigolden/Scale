b595c403995e3b432be3c36f24a8edb25663c237
User <lisaboiko040111@gmail.com>
2024-02-18 10:41:57 +0200

    add text

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ root=Tk()
 root.geometry('750x600')
 root.title('Si_te_d')
 root.iconbitmap('coffee_hot_cafe_icon_261343.ico')
+
+text=Label(text='Gamma editor', font='Arial 16')
+text.pack()
+
 img=PhotoImage(file='Wally.png')
 lb=Label(image=img)
 lb.pack()
