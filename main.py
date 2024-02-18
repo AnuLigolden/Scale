d6252b16ee3ac571cecff261f932837ee04cc225
User <lisaboiko040111@gmail.com>
2024-02-18 10:11:46 +0200

    add scale

--- main.py
+++ main.py
@@ -4,5 +4,16 @@ root=Tk()
 root.geometry('750x600')
 root.title('Si_te_d')
 root.iconbitmap('coffee_hot_cafe_icon_261343.ico')
+img=PhotoImage(file='Wally.png')
+lb=Label(image=img)
+lb.pack()
+
+
+def set_gamma(val):
+    img['gamma']=val
+
+scale=Scale(from_=1 , to=10, length=700, orient=HORIZONTAL, command=set_gamma)
+scale.pack()
+
 
 root=mainloop()
